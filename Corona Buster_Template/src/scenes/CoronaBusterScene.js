2336daf7b6fdca30f5ec2efa24c7ad139441c2d2
Ananda Prema <77527794+menandap@users.noreply.github.com>
2024-08-03 11:30:57 +0800

    up

---
+++ Corona Buster_Template/src/scenes/CoronaBusterScene.js
@@ -0,0 +1,18 @@
+import Phaser from 'phaser'
+
+import FallingObject from '../ui/FallingObject.js'
+import Laser from '../ui/Laser.js'
+import ScoreLabel from '../ui/ScoreLabel.js'
+import LifeLabel from '../ui/LifeLabel.js'
+
+
+export default class CoronaBusterScene extends Phaser.Scene
+{
+	constructor()
+	{
+        super('corona-buster-scene')
+        
+	}
+
+
+}
